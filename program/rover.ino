3da0cf7485ecad0d9fafee4e384aee9a0a1003bf
Aliaksandr Yesis <saschaesa@gmail.com>
2021-06-03 22:35:26 +0300

    Update rover.ino

--- program/rover.ino
+++ program/rover.ino
@@ -8,8 +8,8 @@
 #define MOT_L2 5
 #define MOT_R1 6
 #define MOT_R2 11
-#define BT_TX 10
-#define BT_RX 9
+#define BT_TX 2
+#define BT_RX 12
 #define SPEAKER 13  // пин пищалки
 #define STRIP_PIN 4 // пин ленты
 #define NUMLEDS 6   // кол-во светодиодов
@@ -29,224 +29,220 @@ String string_convert;
 void setup()
 {
 
-    Serial.begin(9600);
-    BTserial.begin(9600);
-    pinMode(MOT_L1, OUTPUT);
-    pinMode(MOT_L2, OUTPUT);
-    pinMode(MOT_R1, OUTPUT);
-    pinMode(MOT_R2, OUTPUT);
+  Serial.begin(9600);
+  BTserial.begin(9600);
+  pinMode(MOT_L1, OUTPUT);
+  pinMode(MOT_L2, OUTPUT);
+  pinMode(MOT_R1, OUTPUT);
+  pinMode(MOT_R2, OUTPUT);
 
-    // Start with drivers off, motors coasting.
-    digitalWrite(MOT_L1, LOW);
-    digitalWrite(MOT_L2, LOW);
-    digitalWrite(MOT_R1, LOW);
-    digitalWrite(MOT_R2, LOW);
+  // Start with drivers off, motors coasting.
+  digitalWrite(MOT_L1, LOW);
+  digitalWrite(MOT_L2, LOW);
+  digitalWrite(MOT_R1, LOW);
+  digitalWrite(MOT_R2, LOW);
 
-    strip.setBrightness(BRIGHTNESS);
-    pinMode(SPEAKER, OUTPUT);
-    randomSeed(analogRead(0));
+  strip.setBrightness(BRIGHTNESS);
+  pinMode(SPEAKER, OUTPUT);
+  randomSeed(analogRead(0));
 }
 
 void loop()
 {
-    parsing(); // функция парсинга
-    if (doneParsing && dataX > JOY_MAX)
-    {
+  parsing(); // функция парсинга
+  if (doneParsing && dataX > JOY_MAX)
+  {
 
-        Serial.println(dataX);
-        motor_max = dataX;
-    }
-    else if (doneParsing)
-    {
+    Serial.println(dataX);
+    motor_max = dataX;
+  }
+  else if (doneParsing)
+  {
 
-        timer = millis();
-        doneParsing = false;
+    timer = millis();
+    doneParsing = false;
 
-        int joystickX = map((dataX), -JOY_MAX, JOY_MAX, -motor_max / 2, motor_max / 2); // сигнал по Х
-        int joystickY = map((dataY), -JOY_MAX, JOY_MAX, -motor_max, motor_max);         // сигнал по Y
+    int joystickX = map((dataX), -JOY_MAX, JOY_MAX, -motor_max / 2, motor_max / 2); // сигнал по Х
+    int joystickY = map((dataY), -JOY_MAX, JOY_MAX, -motor_max, motor_max);         // сигнал по Y
 
-        int dutyR = joystickY - joystickX; // считаем сигнал для правого мотора
-        int dutyL = joystickY + joystickX; // считаем сигнал для левого мотора
+    int dutyR = joystickY - joystickX; // считаем сигнал для правого мотора
+    int dutyL = joystickY + joystickX; // считаем сигнал для левого мотора
 
-        dutyR = constrain(dutyR, -motor_max, motor_max); // ограничиваем значение для dutyR, на случай если dutyR > 255
-        dutyL = constrain(dutyL, -motor_max, motor_max); // ограничиваем значение для dutyL, на случай если dutyL > 255
+    dutyR = constrain(dutyR, -motor_max, motor_max); // ограничиваем значение для dutyR, на случай если dutyR > 255
+    dutyL = constrain(dutyL, -motor_max, motor_max); // ограничиваем значение для dutyL, на случай если dutyL > 255
 
-        rightMotor(dutyR);
-        leftMotor(dutyL);
-    }
-    if (millis() - timer > 5000)
-    {
-        music();
-        timer = millis();
-    }
+    rightMotor(dutyR);
+    leftMotor(dutyL);
+  }
+  if (millis() - timer > 5000)
+  {
+    music();
+    timer = millis();
+  }
 }
 
 void music()
 {
 
-    int K = 2000;
-    switch (random(1, 7))
-    {
+  int K = 2000;
+  switch (random(1, 7))
+  {
 
     case 1:
-        phrase1();
-        break;
+      phrase1();
+      break;
     case 2:
-        phrase2();
-        break;
+      phrase2();
+      break;
     case 3:
-        phrase1();
-        phrase2();
-        break;
+      phrase1();
+      phrase2();
+      break;
     case 4:
-        phrase1();
-        phrase2();
-        phrase1();
-        break;
+      phrase1();
+      phrase2();
+      phrase1();
+      break;
     case 5:
-        phrase1();
-        phrase2();
-        phrase1();
-        phrase2();
-        phrase1();
-        break;
+      phrase1();
+      phrase2();
+      phrase1();
+      phrase2();
+      phrase1();
+      break;
     case 6:
-        phrase2();
-        phrase1();
-        phrase2();
-        break;
-    }
+      phrase2();
+      phrase1();
+      phrase2();
+      break;
+  }
 
-    for (int i = 0; i <= random(3, 9); i++)
-    {
+  for (int i = 0; i <= random(3, 9); i++)
+  {
 
-        printLed();
-        tone(SPEAKER, K + random(-1700, 2000));
-        delay(random(70, 170));
-        strip.clear();
-        noTone(SPEAKER);
-        delay(random(0, 30));
-    }
-
+    printLed();
+    tone(SPEAKER, K + random(-1700, 2000));
+    delay(random(70, 170));
+    strip.clear();
     noTone(SPEAKER);
+    delay(random(0, 30));
+  }
+
+  noTone(SPEAKER);
 }
 
 void parsing()
 {
-    if (BTserial.available() > 0)
-    {                                        // если в буфере есть данные
-        char incomingChar = BTserial.read(); // читаем из буфера
-        if (startParsing)
-        { // начать принятие пакетов
-            if (incomingChar == ' ')
-            {                                   // принят пакет dataX
-                dataX = string_convert.toInt(); // ковертируем принятый пакет в переменную
-                string_convert = "";            // очищаем переменную пакета
-            }
-            else if (incomingChar == ';')
-            {                                   // принят пакет dataY
-                dataY = string_convert.toInt(); // ковертируем принятый пакет в переменную
-                string_convert = "";            // очищаем переменную пакета
-                startParsing = false;           // закончить принятие пакетов
-                doneParsing = true;             // парсинг окончен, можно переходить к движению
-            }
-            else
-            {
-                string_convert += incomingChar; // записываем принятые данные в переменную
-            }
-        }
-        if (incomingChar == '$')
-        {                        // начало парсинга
-            startParsing = true; // начать принятие пакетов
-        }
+  if (BTserial.available() > 0)
+  { // если в буфере есть данные
+    char incomingChar = BTserial.read(); // читаем из буфера
+    if (startParsing)
+    { // начать принятие пакетов
+      if (incomingChar == ' ')
+      { // принят пакет dataX
+        dataX = string_convert.toInt(); // ковертируем принятый пакет в переменную
+        string_convert = "";            // очищаем переменную пакета
+      }
+      else if (incomingChar == ';')
+      { // принят пакет dataY
+        dataY = string_convert.toInt(); // ковертируем принятый пакет в переменную
+        string_convert = "";            // очищаем переменную пакета
+        startParsing = false;           // закончить принятие пакетов
+        doneParsing = true;             // парсинг окончен, можно переходить к движению
+      }
+      else
+      {
+        string_convert += incomingChar; // записываем принятые данные в переменную
+      }
     }
+    if (incomingChar == '$')
+    { // начало парсинга
+      startParsing = true; // начать принятие пакетов
+    }
+  }
 }
 
 void printLed()
 {
-    int num = random(0, 1530);
-    switch (random(1, 4))
-    {
+  int num = random(0, 1530);
+  switch (random(1, 4))
+  {
     case 1:
-        strip.set(0, mWheel(num));
-        strip.set(5, mWheel(num));
-        break;
+      strip.set(0, mWheel(num));
+      strip.set(5, mWheel(num));
+      break;
     case 2:
-        strip.set(1, mWheel(num));
-        strip.set(4, mWheel(num));
-        break;
+      strip.set(1, mWheel(num));
+      strip.set(4, mWheel(num));
+      break;
     case 3:
-        strip.set(2, mWheel(num));
-        strip.set(3, mWheel(num));
-        break;
-    }
-    strip.show();
+      strip.set(2, mWheel(num));
+      strip.set(3, mWheel(num));
+      break;
+  }
+  strip.show();
 }
 
 void rightMotor(size_t _speed)
 {
-    set_motor_pwm(_speed, MOT_R1, MOT_R2);
+  set_motor_pwm(_speed, MOT_R1, MOT_R2);
 }
 
 void leftMotor(size_t _speed)
 {
-    set_motor_pwm(_speed, MOT_L1, MOT_L2);
+  set_motor_pwm(_speed, MOT_L1, MOT_L2);
 }
 
 void set_motor_pwm(int pwm, int IN1_PIN, int IN2_PIN)
 {
-    if (pwm < 0)
-    {
-        analogWrite(IN1_PIN, -pwm);
-        digitalWrite(IN2_PIN, LOW);
-    }
-    else
-    { // stop or forward
-        digitalWrite(IN1_PIN,
-                     Strip.show
-                         strip.show
-
-                             LOW);
-        analogWrite(IN2_PIN, pwm);
-    }
+  if (pwm < 0)
+  {
+    analogWrite(IN1_PIN, -pwm);
+    digitalWrite(IN2_PIN, LOW);
+  }
+  else
+  { // stop or forward
+    digitalWrite(IN1_PIN, LOW);
+    analogWrite(IN2_PIN, pwm);
+  }
 }
 
 void phrase1()
 {
 
-    int k = random(1000, 2000);
-    printLed();
-    for (int i = 0; i <= random(100, 2000); i++)
-    {
+  int k = random(1000, 2000);
+  printLed();
+  for (int i = 0; i <= random(100, 2000); i++)
+  {
 
-        tone(SPEAKER, k + (-i * 2));
-        delay(random(.9, 2));
-    }
-    strip.clear();
-    for (int i = 0; i <= random(100, 1000); i++)
-    {
+    tone(SPEAKER, k + (-i * 2));
+    delay(random(.9, 2));
+  }
+  strip.clear();
+  for (int i = 0; i <= random(100, 1000); i++)
+  {
 
-        tone(SPEAKER, k + (i * 10));
-        delay(random(.9, 2));
-    }
+    tone(SPEAKER, k + (i * 10));
+    delay(random(.9, 2));
+  }
 }
 
 void phrase2()
 {
 
-    int k = random(1000, 2000);
-    printLed();
-    for (int i = 0; i <= random(100, 2000); i++)
-    {
+  int k = random(1000, 2000);
+  printLed();
+  for (int i = 0; i <= random(100, 2000); i++)
+  {
 
-        tone(SPEAKER, k + (i * 2));
-        delay(random(.9, 2));
-    }
-    strip.clear();
-    for (int i = 0; i <= random(100, 1000); i++)
-    {
+    tone(SPEAKER, k + (i * 2));
+    delay(random(.9, 2));
+  }
+  strip.clear();
+  for (int i = 0; i <= random(100, 1000); i++)
+  {
 
-        tone(SPEAKER, k + (-i * 10));
-        delay(random(.9, 2));
-    }
+    tone(SPEAKER, k + (-i * 10));
+    delay(random(.9, 2));
+  }
 }
